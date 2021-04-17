323a71c0cf2a6c639852b09de534de8e34bd39d7
sierraskyewallace <sskwallace@gmail.com>
2021-04-17 15:00:03 -0700

    commit tree 2a847955f085b7a32a7bf5e9bac0ccf0785be5c1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'categories/show'
-
+  resources :accounts
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
   resources :categories
 end
